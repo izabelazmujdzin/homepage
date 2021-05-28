d8c55dfc16c0f089d9a8db0cf6638cb87f89e706
Izabela Żmujdzin <izabela.zmujdzin@gmail.com>
2021-05-28 13:02:52 +0200

    background gradient

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
-
-
 const firstName='Iza'; 
 const age = 27;
 
-console.log(firstName)
-console.log(age)
-console.log(`Hej , nazywam się ${firstName} i mam ${age} lat `)
+console.log(firstName);
+console.log(age);
+
+
+const emptyParagraph = document.querySelector('.section__paragraph--js');
+
+
+
+
+function greetOld(firstName, age) {
+    console.log(`Hej , nazywam się ${firstName} i mam ${age} lat `);
+}
+
+const greet = (firstName, age) => {
+    console.log(`Hej , nazywam się ${firstName} i mam ${age} lat `);
+}
+
+greet('Iza', age);
+
+
+function createContent(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content; 
+}
+
+createContent('.section__paragraph--js', 'Zanim to zapamiętam to miną wieki!');
+
+function helloWorld() {
+    console.log('Witaj świecie!');
+}
+helloWorld();
+
+const goodbye = (firstName, age) => {
+    console.log(`Żegnam się z Wami, dziękuję, że byliście ${firstName}, ${age}`);
+}
+
+goodbye('Iza', age);
